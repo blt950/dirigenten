ec4c43fd7b9779e3bd5effc424909c7c9efb60a0
Blt950 <daniel@lanpost.no>
2017-09-20 19:15:38 +0200

    Changed to SocketIO instead of ExpressJS

--- server.js
+++ server.js
@@ -8,3 +8,34 @@ board.on("ready", function() {
   // Strobe the pin on/off, defaults to 100ms phases
   led.strobe(250);
 });
+
+
+console.log("Server started")
+
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
+var fs = require('fs');
+
+app.listen(8080);
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+io.on('connection', function (socket) {
+	console.log("Started");
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
+     
