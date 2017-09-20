0d8c92a3bfae1e7a0256ac48497a0ad777848a23
Blt950 <daniel@lanpost.no>
2017-09-20 16:33:18 +0200

    Cleanup and added client/server

---
+++ server.js
@@ -0,0 +1,10 @@
+var five = require("./node_modules/johnny-five/lib/johnny-five"),
+    board = new five.Board();
+
+board.on("ready", function() {
+  // Create an Led on pin 13
+  var led = new five.Led(8);
+
+  // Strobe the pin on/off, defaults to 100ms phases
+  led.strobe(250);
+});
