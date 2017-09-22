5422eeb745c8d9f2509310673601eab2e0ab125b
Blt950 <daniel@lanpost.no>
2017-09-22 15:42:14 +0200

    Send data to server, and LED strip via Node working

--- server.js
+++ server.js
@@ -42,24 +42,68 @@ function handler(req, res) {
 
 var strip = null;
 
+var fps = 5;
+
 board.on("ready", function() {
 
 	var led = new five.Led(8);
 	led.strobe(250);
 	
-	strip = new pixel.Strip({
-		board: this,
-		controller: "FIRMATA",
-		strips: [ {pin: 6, length: 9}, ],
-		gamma: 2.8,
-	});
+    // setup the node-pixel strip.
+    strip = new pixel.Strip({
+        data: 6,
+        length: 30, // number of pixels in the strip.
+        board: this,
+        controller: "FIRMATA"
+    });
 
-	strip.on("ready", function(){
+    strip.on("ready", function() {
+        console.log("Strip ready, let's go");
+        dynamicRainbow(fps);
+    });
 
-		strip.color("red");
-		strip.show();
+    function dynamicRainbow( delay ){
+        console.log( 'dynamicRainbow' );
 
-	});
+        var showColor;
+        var cwi = 0; // colour wheel index (current position on colour wheel)
+        var foo = setInterval(function(){
+            if (++cwi > 255) {
+                cwi = 0;
+            }
+
+            for(var i = 0; i < strip.length; i++) {
+                showColor = colorWheel( ( cwi+i+5 ) & 255 );
+                strip.pixel( i ).color( showColor );
+            }
+            strip.show();
+        }, 1000/delay);
+    }
+
+    // Input a value 0 to 255 to get a color value.
+    // The colors are a transition r - g - b - back to r.
+    function colorWheel( WheelPos ){
+        var r,g,b;
+        WheelPos = 255 - WheelPos;
+
+        if ( WheelPos < 85 ) {
+            r = 255 - WheelPos * 3;
+            g = 0;
+            b = WheelPos * 3;
+        } else if (WheelPos < 170) {
+            WheelPos -= 85;
+            r = 0;
+            g = WheelPos * 3;
+            b = 255 - WheelPos * 3;
+        } else {
+            WheelPos -= 170;
+            r = WheelPos * 3;
+            g = 255 - WheelPos * 3;
+            b = 0;
+        }
+        // returns a string with the rgb value to be used as the parameter
+        return "rgb(" + r +"," + g + "," + b + ")";
+    }
 
 });
 
@@ -70,7 +114,7 @@ board.on("ready", function() {
 
 io.on('connection', function (socket) {
 	console.log("Client Connected");
-	socket.on('volumeUpdate', function (data) {
+	socket.on('clientPackage', function (data) {
 		console.log(data);
 	});
 });
